b0813b4cf4e3c2d71548a544b1872114ea405392
jmarkos <markos@joomlashack.com>
2016-07-27 02:13:08 -0600

    done 8

--- src/8.cs
+++ src/8.cs
@@ -5,12 +5,17 @@ public class Program
 	public static void Main()
 	{
     int[] table = new int[10];
+		int		tableLength = table.Length;
 
 		Console.WriteLine("Ingrese numero: ");
 		int num = Int32.Parse(Console.ReadLine ());
     
-    for (int i = 1; i <= table.Length; i++)
+		Console.WriteLine(tableLength);
+
+    for (int i=0; i <= tableLength; i++)
     {
+			table[i] = i + 1;
+			Console.WriteLine(i);
       Console.WriteLine(num + " * " + table[i] + " = " + (num * table[i]));
     }
 	}
