14c69f15e69ffc1041184f7d722aa0ca0a298876
jmarkos <jkmarcosb@gmail.com>
2016-07-27 01:53:16 -0600

    adding 8

---
+++ src/8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+    int[] table = new int[10];
+
+		Console.WriteLine("Ingrese numero: ");
+		int num = Int32.Parse(Console.ReadLine ());
+
+    for (int i; i <= table.Lengdth; i++)
+    {
+      Console.WriteLine(num + "*" + table[i] + "=" + num * table[i]);
+    }
+	}
+}
