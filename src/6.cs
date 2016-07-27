d8d3b3ee11ba7eda81245d873f42aa3ed0e1ea5a
jmarkos <jkmarcosb@gmail.com>
2016-07-27 01:41:25 -0600

    fix syntax error

--- src/6.cs
+++ src/6.cs
@@ -5,18 +5,21 @@ public class Program
 	public static void Main()
 	{
 		int operation;
-
+											
 		Console.WriteLine("Ingrese num1: ");
-		int num1 = Int32.Pare(Console.ReadLine());
-		
+		int num1 = Int32.Parse(Console.ReadLine());
+																	
 		Console.WriteLine("Ingrese num2: ");
-		int num2 = Int32.Pare(Console.ReadLine());
-		
-		Console.WriteLine("Ingrese num3: ");
-		int num3 = Int32.Pare(Console.ReadLine());
-
+   	int num2 = Int32.Parse(Console.ReadLine());
+																										
+    Console.WriteLine("Ingrese num3: ");
+		int num3 = Int32.Parse(Console.ReadLine());
+																											
 		operation = num1 * num2 * num3;
-
-		Console.Write("El resultado; " + operation);
+																													
+	  Console.Write("El resultado; " + operation);
 	}
 }
+					
+					
+					
