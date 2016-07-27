eac3236f1490fd89cacfd4c07906caf1060dae9b
J0seMark0s <jkmarcosb@gmail.com>
2016-07-27 02:32:10 -0600

    fix limit array and rm log

--- src/8.cs
+++ src/8.cs
@@ -1,22 +1,19 @@
-﻿using System;
+using System;
 
 public class Program
 {
 	public static void Main()
 	{
-    int[] table = new int[10];
-		int		tableLength = table.Length;
-
+	    int[] table = new int[10];
+		int	tableLength = table.Length;
+		
 		Console.WriteLine("Ingrese numero: ");
 		int num = Int32.Parse(Console.ReadLine ());
-    
-		Console.WriteLine(tableLength);
-
-    for (int i=0; i <= tableLength; i++)
-    {
+		
+    	for (int i=0; i < tableLength; i++)
+    	{
 			table[i] = i + 1;
-			Console.WriteLine(i);
-      Console.WriteLine(num + " * " + table[i] + " = " + (num * table[i]));
-    }
+			Console.WriteLine(num + " * " + table[i] + " = " + (num * table[i]));
+    	}
 	}
 }
