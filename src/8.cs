dd1545c844acb8530d66c84cf74e8900845518b8
jmarkos <jkmarcosb@gmail.com>
2016-07-28 16:00:15 -0600

    testing configuration spaces 2

--- src/8.cs
+++ src/8.cs
@@ -5,15 +5,15 @@ public class Program
 	public static void Main()
 	{
 	  int[] table = new int[10];
-		int	tableLength = table.Length;
-		
-		Console.WriteLine("Ingrese numero: ");
-		int num = Int32.Parse(Console.ReadLine ());
+    int	tableLength = table.Length;
+    
+    Console.WriteLine("Ingrese numero: ");
+    int num = Int32.Parse(Console.ReadLine ());
     
     for (int i=0; i < tableLength; i++)
-    	{
-        table[i] = i + 1;
-        Console.WriteLine(num + " * " + table[i] + " = " + (num * table[i]));
-    	}
+    {
+      table[i] = i + 1;
+      Console.WriteLine(num + " * " + table[i] + " = " + (num * table[i]));
+    }
 	}
 }
